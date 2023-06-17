f53391ffcf97468dec6394f8d07f9810ed8117b3
Srinanda Kishore Yallapragada <srinanda.yallapragada@gmail.com>
2023-06-17 14:47:52 +0530

    update toggle graphics to work on hyprland and use correct arguments for supergfx ctl and asusctl

--- toggle_graphics.sh
+++ toggle_graphics.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #This script uses supergfxctl to toggle between graphics modes of integrated and hybrid
-#when the mute button (XF86AudioMute) is pressed on ASUS Zephyrus G14 laptop with nvidia 3060
+#when the ROG button (XF86Launch1) is pressed on ASUS Zephyrus G14 laptop with nvidia 3060
+#Assumes hyprland. line "hyprctl exit" needs to be changed depending on which env this is used
 
 cur_status=$(supergfxctl -g)
 
-if [ "$cur_status" = "integrated" ]; then
-  supergfxctl -m hybrid
-  notify-send "switching to hybrid mode and logging out"
-  bspc quit
-elif [ "$cur_status" = "hybrid" ]; then
-  supergfxctl -m integrated
-  notify-send "switching to integrated mode and logging out"
-  bspc quit
+if [ "$cur_status" = "Integrated" ]; then
+  supergfxctl -m Hybrid
+  asusctl profile -P Performance
+  notify-send "switching to hybrid mode, performance profile and logging out"
+  sleep 1
+  hyprctl dispatch exit
+elif [ "$cur_status" = "Hybrid" ]; then
+  supergfxctl -m Integrated
+  asusctl profile -P Quiet
+  notify-send "switching to integrated mode, quiet profile and logging out"
+  sleep 1
+  hyprctl dispatch exit
 else 
   notify-send "This program only supports toggling between integrated and hybird modes in supergfxctl"
 fi
